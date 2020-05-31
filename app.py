4bf29b22954c7064e4a738d13684abd8898c0f7e
ds-ravipatel <ds.ravi.patel@gmail.com>
2020-05-31 23:32:52 +0530

    added churn prediction files

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify, render_template
 import numpy as np
 import pickle
+import keras.models
+from sklearn.externals.joblib import dump, load
 
 app = Flask(__name__)
 sal_model = pickle.load(open('Poly_LR_Model.pkl', 'rb'))
+churn_model = models.load_model('ChurnPredModel.h5')
+churn_ss = load('std_scaler.bin')
 
 @app.route('/')
 def home():
@@ -30,6 +34,23 @@ def predict():
     prediction = sal_model.predict(input)
     output = round(prediction[0], 2)
     return render_template('SalaryPrediction.html', prediction_text='Salary at this level should be $ {}'.format(output))
+
+@app.route('/ChurnPrediction')
+def churnpred():
+    return render_template('ChurnPrediction.html')
+
+@app.route('/ChurnPrediction/Predict', methods=['POST'])
+def churnpredict():
+    #using below - parameters will be stored in list
+    int_features = [int(x) for x in request.form.values()]
+    int_features = np.array(int_features)
+    #convert 1D array to 2D array.
+    input = int_features.reshape(1,8)
+    #do standard scaling of input
+    input = churn_ss.transform(input)
+    prediction = churn_model.predict(input)
+    output = prediction>0.5
+    return render_template('ChurnPrediction.html', prediction_text='Exited ? '.format(output), percent_text='Chances are : '.format(prediction))
 
 if __name__ == "__main__":
     app.run(debug=True)
